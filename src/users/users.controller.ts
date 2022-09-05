285f20949d1dbfd7364ebacc5a0154e7fbaa3141
Selim Yeom <selim.yeom@lfin.kr>
2022-09-05 18:48:03 +0900

    🎨 4.3. 유저 서비스에 회원가입 로직 구현하기  - user 컨트롤러에 service 연결

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -7,31 +7,29 @@ import { UsersService } from './users.service';
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
   @Post()
   async createUser(@Body() dto: CreateUserDto): Promise<void> {
-    console.log(dto);
+    const { name, email, password } = dto;
+    await this.usersService.createUser(name, email, password);
   }
 
   @Post('/email-verify')
   async verifyEmail(@Body() dto: VerifyEmailDto): Promise<string> {
-    console.log(dto);
-    return;
+    const { signupVerifyToken } = dto;
+
+    return await this.usersService.verifyEmail(signupVerifyToken);
   }
 
   @Post('/login')
   async login(@Body() dto: UserLoginDto): Promise<string> {
-    console.log(dto);
-    return;
+    const { email, password } = dto;
+
+    return await this.usersService.login(email, password);
   }
 
   @Get('/:id')
-  async getUserInfo(@Param('id') userId: string): Promise<string> {
-    console.log(userId);
-    return;
-  }
-
-  @Delete('/:id')
-  remove(@Param('id') id: string) {
-    return this.usersService.remove(+id);
+  async getUserInfo(@Param('id') userId: string): Promise<UserInfo> {
+    return await this.usersService.getUserInfo(userId);
   }
 }

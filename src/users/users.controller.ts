c4a31745957af6f72fee0ac47fe478be83477778
Selim Yeom <selim.yeom@lfin.kr>
2022-09-05 17:24:04 +0900

    ✨ 3.2 유저 서비스의 인터페이스  - 인터페이스로 지정한 method 및 dto 생성

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user-dto';
+import { UserLoginDto } from './dto/user-login-dto';
+import { VerifyEmailDto } from './dto/verify-email-dto';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Post()
+  async createUser(@Body() dto: CreateUserDto): Promise<void> {
+    console.log(dto);
+  }
+
+  @Post('/email-verify')
+  async verifyEmail(@Body() dto: VerifyEmailDto): Promise<string> {
+    console.log(dto);
+    return;
+  }
+
+  @Post('/login')
+  async login(@Body() dto: UserLoginDto): Promise<string> {
+    console.log(dto);
+    return;
+  }
+
+  @Get('/:id')
+  async getUserInfo(@Param('id') userId: string): Promise<string> {
+    console.log(userId);
+    return;
+  }
+}
